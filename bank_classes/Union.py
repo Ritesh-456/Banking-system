2c41ca3292bb9bf2e22859f5c3d667dd54da63c0
Ritesh Brahmachari <136628905+Ritesh-456@users.noreply.github.com>
2025-07-25 17:37:03 +0530

    1. create a login portal where new  customer can create a new bank account 2.  new customer detail is store within a .txt file according to the pertiular bank name
    ----- future ----
    
    3. login portal for existing customer those who re  in different .txt file

---
+++ bank_classes/Union.py
@@ -0,0 +1,17 @@
+class Union:
+    def __init__(self, user_data):
+        self.data = user_data
+
+    def deposit(self, amount):
+        self.data["balance"] += amount
+        print(f"₹{amount} deposited. New Balance: ₹{self.data['balance']}")
+
+    def withdraw(self, amount):
+        if amount > self.data["balance"]:
+            print("❌ Insufficient Balance.")
+        else:
+            self.data["balance"] -= amount
+            print(f"₹{amount} withdrawn. Remaining Balance: ₹{self.data['balance']}")
+
+    def check_balance(self):
+        print(f"🔎 Your Balance: ₹{self.data['balance']}")
